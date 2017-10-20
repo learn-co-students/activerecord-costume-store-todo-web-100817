64b7e1836ca1a1ddeb6edd098a5645a47f7b8ab8
flored27 <dflores@fandm.edu>
2017-10-20 10:48:54 -0400

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,17 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |x|
+      x.text :name
+      x.text :location
+      x.integer :costume_inventory
+      x.integer :num_of_employees
+      x.boolean :still_in_business
+      x.datetime :opening_time
+      x.datetime :closing_time
+
+      x.timestamps
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |x|
+      x.text :name
+      x.text :location
+      x.text :theme
+      x.integer :price
+      x.boolean :family_friendly
+      x.datetime :opening_date
+      x.datetime :closing_date
+      x.text :description
+
+      x.timestamps
+    end
+  end
+end
